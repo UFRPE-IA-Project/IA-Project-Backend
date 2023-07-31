7cfd1fcaa9716e17be09020978cc0b0b38c3da1d
Kleyton Clementino <kleytondread@gmail.com>
2023-07-31 15:01:10 -0300

    Upload do projeto de Entities

---
+++ IAE.Entidades/Entities/Resposta.cs
@@ -0,0 +1,9 @@
+﻿namespace IAE.Entidades.Entidades
+{
+	public class Resposta
+	{
+        public string? EnunciadoResposta { get; set; }
+        public bool IsRespostaCorreta { get; set; }
+        public int? IdQuestao { get; set; }
+    }
+}
