6c727afc89f1116890625e08a228c6feacf807ec
kleytondread <kleytondread@gmail.com>
2023-07-31 19:13:02 -0300

    voltando a nome original.

---
+++ Entidades/Entities/Questao.cs
@@ -0,0 +1,16 @@
+﻿using IAE.Entities.Entities;
+
+namespace IAE.Entidades.Entidades
+{
+    public class Questao : BaseEntity
+    {
+        public Questao()
+        {
+            Enunciado = string.Empty;
+            Respostas = new List<Resposta>();
+        }
+
+        public string Enunciado { get; set; }
+        public List<Resposta> Respostas { get; set; }
+    }
+}
