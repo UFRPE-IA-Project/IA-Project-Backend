e99471c6f6717ce6f69dd2e1758150c2c3111b20
v-anunciacao <113193512+v-anunciacao@users.noreply.github.com>
2023-08-09 18:16:44 -0300

    Implementação de RespostaService.cs
    Repositório de Respostas: A classe usa um repositório de respostas (IRespostaRepository) para realizar operações de acesso a dados. Isso requer que a interface de repositório correspondente esteja definida e implementada.
    Métodos: Os métodos fornecem operações para obter, adicionar e apagar respostas, utilizando o repositório para acesso a dados.

--- IAE.Services/Services/RespostaService.cs
+++ IAE.Services/Services/RespostaService.cs
@@ -1,13 +1,38 @@
-﻿using IAE.Services.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using IAE.Repositorio.Interfaces; // Para acesso ao repositório
+using IAE.Entidades.Entities;     // Para acesso às entidades
 
 namespace IAE.Services.Services
 {
-	public class RespostaService : IRespostaService
-	{
-	}
+    public class RespostaService : IRespostaService
+    {
+        private readonly IRespostaRepository _respostaRepository; // Repositório para acesso às respostas
+
+        // Construtor para injeção de dependência
+        public RespostaService(IRespostaRepository respostaRepository)
+        {
+            _respostaRepository = respostaRepository;
+        }
+
+        // Obtém uma resposta pelo ID
+        public Resposta GetResposta(int id)
+        {
+            return _respostaRepository.GetById(id);
+        }
+
+        // Adiciona uma nova resposta
+        public void AdicionarResposta(Resposta resposta)
+        {
+            _respostaRepository.Add(resposta);
+        }
+
+        // Apaga uma resposta pelo ID
+        public void ApagarResposta(int id)
+        {
+            var resposta = _respostaRepository.GetById(id);
+            if (resposta != null)
+            {
+                _respostaRepository.Delete(resposta);
+            }
+        }
+    }
 }
