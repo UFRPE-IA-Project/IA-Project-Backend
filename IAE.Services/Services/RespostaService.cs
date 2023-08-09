de3bcf49bd7eb339d76d4ac43e48f78639fa94cf
KleytonClementino <kleytondread@gmail.com>
2023-08-09 18:39:09 -0300

    remoção de comentários. #14

--- IAE.Services/Services/RespostaService.cs
+++ IAE.Services/Services/RespostaService.cs
@@ -6,27 +6,23 @@ namespace IAE.Services.Services
 {
     public class RespostaService : IRespostaService
     {
-        private readonly IRespostaRepository _respostaRepository; // Repositório para acesso às respostas
+        private readonly IRespostaRepository _respostaRepository;
 
-        // Construtor para injeção de dependência
         public RespostaService(IRespostaRepository respostaRepository)
         {
             _respostaRepository = respostaRepository;
         }
 
-        // Obtém uma resposta pelo ID
         public Resposta GetResposta(int id)
         {
             return _respostaRepository.FindById(id);
         }
 
-        // Adiciona uma nova resposta
         public void AdicionarResposta(Resposta resposta)
         {
             _respostaRepository.Add(resposta);
         }
 
-        // Apaga uma resposta pelo ID
         public void ApagarResposta(int id)
         {
             var resposta = _respostaRepository.FindById(id);
