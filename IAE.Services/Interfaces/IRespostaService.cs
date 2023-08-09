c4a90eadc593c8f1574c463c6bd3a00ee8ca0c19
v-anunciacao <113193512+v-anunciacao@users.noreply.github.com>
2023-08-09 18:25:24 -0300

    ImplementaçãoIRespostaService.cs
    A atualização proposta para a interface IRespostaService garante que ela esteja alinhada com a implementação da classe RespostaService.
    
    - Métodos: A interface foi atualizada para incluir os métodos para obter, adicionar e apagar respostas. Esses métodos correspondem à implementação proposta na classe RespostaService.
    
    Essas atualizações garantem a consistência entre a interface e a implementação, definindo claramente o contrato para o serviço de respostas e fornecendo a lógica necessária para gerenciar respostas dentro do sistema.

--- IAE.Services/Interfaces/IRespostaService.cs
+++ IAE.Services/Interfaces/IRespostaService.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using IAE.Entidades.Entities; // Para acesso às entidades
 
 namespace IAE.Services.Interfaces
 {
-	public interface IRespostaService
-	{
-	}
+    public interface IRespostaService
+    {
+        // Obtém uma resposta pelo ID
+        Resposta GetResposta(int id);
+
+        // Adiciona uma nova resposta
+        void AdicionarResposta(Resposta resposta);
+
+        // Apaga uma resposta pelo ID
+        void ApagarResposta(int id);
+    }
 }
