91fe2c95af997b64b6bb444b41e158827fccf371
v-anunciacao <113193512+v-anunciacao@users.noreply.github.com>
2023-08-11 00:32:30 -0300

    Update TurmaRepository.cs

--- IAE.Repositorio/Repositories/TurmaRepository.cs
+++ IAE.Repositorio/Repositories/TurmaRepository.cs
@@ -1,14 +1,18 @@
+using IAE.Entidades.Entidades;
+using IAE.Repositorio.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using IAE.Repositorio.Interfaces;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace IAE.Repositorio.Repositories
 {
-    public class TurmaRepository : ITurmaRepository
+    public class TurmaRepository : BaseRepository<Turma>, ITurmaRepository
     {
         public List<Turma> BuscarTurmasPorUsuario(Usuario usuario)
         {
-            // Lógica para buscar turmas do usuário no banco de dados
+            // LÃ³gica para buscar turmas do usuÃ¡rio no banco de dados
             // Exemplo:
             // return _context.Turmas.Where(t => t.UsuarioId == usuario.Id).ToList();
         }
