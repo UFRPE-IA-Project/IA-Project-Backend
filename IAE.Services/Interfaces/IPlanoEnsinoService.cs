492bbebbfb3281c388919c053614ae8a1d030886
KleytonClementino <kleytondread@gmail.com>
2023-08-16 17:34:50 -0300

    Adição full CRUD Plano Ensino

---
+++ IAE.Services/Interfaces/IPlanoEnsinoService.cs
@@ -0,0 +1,21 @@
+﻿using IAE.Entities.DTO;
+using IAE.Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IAE.Services.Interfaces
+{
+	public interface IPlanoEnsinoService
+	{
+		PlanoEnsino AtualizarPlanoEnsino(int idPlano, PlanoEnsinoDTO dto);
+		void CriarMultiplosPlanosEnsino(List<PlanoEnsinoDTO> dtos);
+		PlanoEnsino CriarPlanoEnsino(PlanoEnsinoDTO dto);
+		void ExcluirPlanoEnsino(int id);
+		PlanoEnsino GetPlanoEnsino(int id);
+		List<PlanoEnsino> GetPlanosEnsino(List<int> ids);
+		List<PlanoEnsino> ObterTodosPlanosEnsino();
+	}
+}
---
+++ IAE.Services/Services/PlanoEnsinoService.cs
@@ -0,0 +1,120 @@
+﻿using IAE.Entities.DTO;
+using IAE.Entities.Entities;
+using IAE.Repository.Interfaces;
+using IAE.Services.Interfaces;
+
+namespace IAE.Services.Services
+{
+	public class PlanoEnsinoService : IPlanoEnsinoService
+	{
+		private readonly IPlanoEnsinoRepository _planoEnsinoRepository;
+
+		public PlanoEnsinoService(IPlanoEnsinoRepository planoEnsinoRepository)
+		{
+			_planoEnsinoRepository = planoEnsinoRepository;
+
+		}
+
+		public PlanoEnsino CriarPlanoEnsino(PlanoEnsinoDTO dto)
+		{
+			var planoEnsino = CriarNovoPlanoEnsinoPeloDto(dto);
+
+			var planoEnsinoDb = _planoEnsinoRepository.Insert(planoEnsino);
+
+			if (dto.ReferenciasBibliograficas?.Any() == true || dto.TopicosAbordados?.Any() == true)
+			{
+				AdicionarReferenciasETopicos(dto);
+
+				//Atualizar as listas do plano de ensino
+				planoEnsinoDb = _planoEnsinoRepository.FindById(planoEnsinoDb.Id!.Value);
+			}
+
+			return planoEnsinoDb;
+		}
+
+		public void CriarMultiplosPlanosEnsino(List<PlanoEnsinoDTO> dtos)
+		{
+			var planosEnsino = new List<PlanoEnsino>();
+
+			dtos.ForEach(d =>
+			{
+				planosEnsino.Add(CriarNovoPlanoEnsinoPeloDto(d));
+			});
+
+			var linhasAfetadas = _planoEnsinoRepository.Insert(planosEnsino);
+			if (linhasAfetadas < 1)
+			{
+				throw new Exception("Houve algum problema com a inserção dos planos de ensino.");
+			}
+		}
+
+		public PlanoEnsino GetPlanoEnsino(int id)
+		{
+			var PlanoEnsino = _planoEnsinoRepository.FindById(id);
+
+			return PlanoEnsino;
+		}
+
+		public List<PlanoEnsino> GetPlanosEnsino(List<int> ids)
+		{
+			var PlanosEnsino = _planoEnsinoRepository.FindByIds(ids);
+
+			return PlanosEnsino.ToList();
+		}
+
+		public List<PlanoEnsino> ObterTodosPlanosEnsino()
+		{
+			var PlanosEnsino = _planoEnsinoRepository.FindAll();
+
+			return PlanosEnsino.ToList();
+		}
+
+		public PlanoEnsino AtualizarPlanoEnsino(int idPlano, PlanoEnsinoDTO dto)
+		{
+			var planoEnsino = _planoEnsinoRepository.FindById(idPlano);
+
+			planoEnsino.Nome = dto.Nome;
+			planoEnsino.NomeDisciplina = dto.NomeDisciplina;
+			planoEnsino.Escolaridade = dto.Escolaridade;
+			planoEnsino.CargaHoraria = dto.CargaHoraria;
+
+			var planoEnsinoDb = _planoEnsinoRepository.Update(planoEnsino);
+
+			return planoEnsinoDb;
+		}
+
+		public void ExcluirPlanoEnsino(int id)
+		{
+			var linhasExcluidas = _planoEnsinoRepository.Delete(id);
+
+			if (linhasExcluidas < 1)
+			{
+				throw new ArgumentException("O plano de ensino não existe no banco de dados.");
+			}
+		}
+
+		private PlanoEnsino CriarNovoPlanoEnsinoPeloDto(PlanoEnsinoDTO dto)
+		{
+			return new PlanoEnsino
+			{
+				Nome = dto.Nome,
+				NomeDisciplina = dto.NomeDisciplina,
+				Escolaridade = dto.Escolaridade,
+				CargaHoraria = dto.CargaHoraria
+			};
+		}
+
+		private void AdicionarReferenciasETopicos(PlanoEnsinoDTO dto)
+		{
+			if (dto.ReferenciasBibliograficas?.Any() == true)
+			{
+				//TODO: Chamar método de adicionar referencias bibliograficas
+			}
+
+			if (dto.TopicosAbordados?.Any() == true)
+			{
+				//TODO: Chamar método de adicionar topicos abordados.
+			}
+		}
+	}
+}
