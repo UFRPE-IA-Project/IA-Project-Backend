f82767c7314139e0d939414274f335dfaf6ff3f3
Diego passos <die-gops@hotmail.com>
2023-08-16 20:53:35 -0300

    CRUD de Avaliações e Turma implementado, respectivos serviços criado na service de Avaliação e turma, Controller de Turma implementado

--- IAE.Services/Interfaces/ITurmaService.cs
+++ IAE.Services/Interfaces/ITurmaService.cs
@@ -7,5 +7,9 @@ namespace IAE.Services.Interfaces
     {
         List<Turma> BuscarTurmasPorUsuario(Usuario usuario);
         Turma BuscarTurmaPorId(int id);
+        void AdicionarTurma(Turma turma);
+        void AtualizarTurma(Turma turma);
+        void ExcluirTurma(int id);
+        List<Turma> ObterTodasTurmas();
     }
 }
