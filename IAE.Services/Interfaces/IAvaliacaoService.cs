95234d2e11c7e9adf517a6cd4a9fb85537307555
diegopss <die-gops@hotmail.com>
2023-08-16 16:23:19 -0300

    implementando metodos do CRUD na Service de Avaliacoes

--- IAE.Services/Interfaces/IAvaliacaoService.cs
+++ IAE.Services/Interfaces/IAvaliacaoService.cs
@@ -1,12 +1,17 @@
-﻿using IAE.Entities.Entities;
+﻿using IAE.Entities.DTO;
+using IAE.Entities.Entities;
 using IAE.Entities.Enumarations;
 
 namespace IAE.Services.Interfaces
 {
     public interface IAvaliacaoService
     {
-      public Avaliacao GerarSimulado(int turmaId);
-      public Avaliacao GerarProva(int turmaId);
+        Avaliacao GetAvaliacao(int id);
+        List<Avaliacao> ObterTodasAvaliacoes();
+        void ExcluirAvaliacao(int idAvaliacao);
+        Avaliacao AtualizarAvaliacao(int idAvaliacao, AvaliacaoDTO avaliacaoAtualizada);
+        public Avaliacao GerarSimulado(int turmaId);
+        public Avaliacao GerarProva(int turmaId);
 
     }
 
