8c89868e2166786deb4f55da930b6e6ffd534756
v-anunciacao <113193512+v-anunciacao@users.noreply.github.com>
2023-08-20 01:25:11 -0300

    Build realizado, CRUD_Avaliação para serv e repo

--- IAE.Repositorio/Repositories/AvaliacaoRepository.cs
+++ IAE.Repositorio/Repositories/AvaliacaoRepository.cs
@@ -5,6 +5,8 @@ using IAE.Services.Services;
 using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
+using System.Data.SQLite;
+using Dapper;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,25 +15,93 @@ namespace IAE.Repository.Repositories
 {
     public class AvaliacaoRepository : BaseRepository<Avaliacao>, IAvaliacaoRepository
     {
-        public AvaliacaoRepository(IConfiguration config) :
-            base(config)
+        public AvaliacaoRepository(IConfiguration config) : base(config) { }
+
+        public override Avaliacao Insert(Avaliacao item)
         {
-                
+            using (var connection = new SQLiteConnection(_connectionString))
+            {
+                connection.Open();
+                var query = "INSERT INTO Avaliacao (Descricao, Data, Nota) VALUES (@Descricao, @Data, @Nota); SELECT last_insert_rowid();";
+                item.Id = connection.ExecuteScalar<int>(query, item);
+                return item;
+            }
         }
 
-		public override Avaliacao Insert(Avaliacao item)
-		{
-			throw new NotImplementedException();
-		}
+        public override int Insert(IList<Avaliacao> items)
+        {
+            int count = 0;
+            using (var connection = new SQLiteConnection(_connectionString))
+            {
+                connection.Open();
+                var query = "INSERT INTO Avaliacao (Descricao, Data, Nota) VALUES (@Descricao, @Data, @Nota)";
+                foreach (var item in items)
+                {
+                    count += connection.Execute(query, item);
+                }
+            }
+            return count;
+        }
 
-		public override int Insert(IList<Avaliacao> items)
-		{
-			throw new NotImplementedException();
-		}
+        public override Avaliacao Update(Avaliacao item)
+        {
+            using (var connection = new SQLiteConnection(_connectionString))
+            {
+                connection.Open();
+                var query = "UPDATE Avaliacao SET Descricao = @Descricao, Data = @Data, Nota = @Nota WHERE Id = @Id";
+                connection.Execute(query, item);
+                return item;
+            }
+        }
 
-		public override Avaliacao Update(Avaliacao item)
-		{
-			throw new NotImplementedException();
-		}
-	}
+        public IList<Avaliacao> FindAll()
+        {
+            using (var connection = new SQLiteConnection(_connectionString))
+            {
+                connection.Open();
+                var query = "SELECT * FROM Avaliacao";
+                return connection.Query<Avaliacao>(query).ToList();
+            }
+        }
+
+        public Avaliacao FindById(int key)
+        {
+            using (var connection = new SQLiteConnection(_connectionString))
+            {
+                connection.Open();
+                var query = "SELECT * FROM Avaliacao WHERE Id = @Id";
+                return connection.QueryFirstOrDefault<Avaliacao>(query, new { Id = key });
+            }
+        }
+
+        public IList<Avaliacao> FindByIds(IEnumerable<int> keys)
+        {
+            using (var connection = new SQLiteConnection(_connectionString))
+            {
+                connection.Open();
+                var query = "SELECT * FROM Avaliacao WHERE Id IN @Ids";
+                return connection.Query<Avaliacao>(query, new { Ids = keys }).ToList();
+            }
+        }
+
+        public int Delete(int id)
+        {
+            using (var connection = new SQLiteConnection(_connectionString))
+            {
+                connection.Open();
+                var query = "DELETE FROM Avaliacao WHERE Id = @Id";
+                return connection.Execute(query, new { Id = id });
+            }
+        }
+
+        public int DeleteItens(IEnumerable<int> ids)
+        {
+            using (var connection = new SQLiteConnection(_connectionString))
+            {
+                connection.Open();
+                var query = "DELETE FROM Avaliacao WHERE Id IN @Ids";
+                return connection.Execute(query, new { Ids = ids });
+            }
+        }
+    }
 }
--- IAE.Services/Services/AvaliacaoService.cs
+++ IAE.Services/Services/AvaliacaoService.cs
@@ -18,25 +18,46 @@ namespace IAE.Services.Services
         {
             _avaliacaoRepository = avaliacaoRepository;
         }
+
         public Avaliacao GerarSimulado(int turmaId)
         {
-            
             Avaliacao avaliacao = new Avaliacao();
             avaliacao.TipoAvaliacao = TipoAvaliacao.Simulado;
-            
-            
-            
             return avaliacao;
         }
+
         public Avaliacao GerarProva(int turmaId)
         {
-
             Avaliacao avaliacao = new Avaliacao();
             avaliacao.TipoAvaliacao = TipoAvaliacao.Prova;
-           
-
-
             return avaliacao;
+        }
+
+        // Métodos CRUD
+
+        public Avaliacao Create(Avaliacao avaliacao)
+        {
+            return _avaliacaoRepository.Insert(avaliacao);
+        }
+
+        public IEnumerable<Avaliacao> ReadAll()
+        {
+            return _avaliacaoRepository.FindAll();
+        }
+
+        public Avaliacao ReadById(int id)
+        {
+            return _avaliacaoRepository.FindById(id);
+        }
+
+        public Avaliacao Update(Avaliacao avaliacao)
+        {
+            return _avaliacaoRepository.Update(avaliacao);
+        }
+
+        public void Delete(int id)
+        {
+            _avaliacaoRepository.Delete(id);
         }
     }
 }
